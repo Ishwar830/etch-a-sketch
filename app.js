60451a1bd5e95f234eb8fdf5c2215f6e2a5320ea
IshwarSuthar <easwersuthar830@gmail.com>
2025-04-28 05:28:46 +0000

    Add hover-effect to color cells

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const SIZE = 16;
 
 generateGrid(SIZE);
 
+gridContainer.addEventListener('mouseover', (e) => {
+    if(e.target.classList.contains('cell')){
+        colorCell(e.target);
+    }
+})
+
+function colorCell(cellElement){
+    cellElement.style.backgroundColor = `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`; 
+    console.log(cellElement.style.backgroundColor);
+}
+
+
+function getRandomRGB(){
+    const MAX_VAL = 256;
+    return Math.floor(Math.random() * MAX_VAL);
+}
+
+
 function generateGrid(size){
     gridContainer.textContent = '';
 
